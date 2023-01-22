ce18e692ebc4b8477159fb11eb4d2279548bb00e
Pikkuherkko <juhhoh95@hotmail.com>
2023-01-23 00:00:40 +0200

    comments

--- L1/contracts/Verifier_L1.sol
+++ L1/contracts/Verifier_L1.sol
@@ -5,6 +5,11 @@ pragma solidity >=0.7.0 <0.9.0;
 import "../interfaces/IStarknetCore.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+
+/// @title Verifier_L1
+/// @author Pikkuherkko
+/// @notice Sends user's L1 address to Verifier on Starknet 
 contract Verifier_L1 is Ownable {
     IStarknetCore private starknetCore;
     uint256 private verifier_L2;
@@ -12,20 +17,24 @@ contract Verifier_L1 is Ownable {
 
     event messageSentToStarkNet(uint256 l2_user);
 
+    /// @dev Starknet address (uint256) by L1 address
     mapping(address => uint256) public l2_address;
 
     constructor(address _starknetCore) {
         starknetCore = IStarknetCore(_starknetCore);
     } 
 
+    /// @dev setting the function name of the Starknet contract
     function setSelector(uint256 _selector) external onlyOwner {
         selector = _selector;
     }
 
+    /// @dev setting the address of the Starknet verifier contract
     function setL2Verifier(uint256 _l2_verifier_contract) external onlyOwner {
         verifier_L2 = _l2_verifier_contract;
     }
 
+    /// @dev sends a message containing user's Starknet address
     function verifyOnL2(uint256 _l2_user) external {
         uint256[] memory payload = new uint256[](2);
         payload[0] = uint256(uint160(msg.sender));
@@ -39,6 +48,7 @@ contract Verifier_L1 is Ownable {
         emit messageSentToStarkNet(_l2_user);
     } 
 
+    /// @dev gets Starknet address claimed by L1 address
     function getL2Address(address L1_address) public view returns (uint256) {
         return l2_address[L1_address];
     }
